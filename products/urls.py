f02a56516c5e55aec83815027fd04d23fbc5f881
nahyunkeem <kim1990t@naver.com>
2024-08-22 13:15:16 +0900

    나현

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,13 @@
-from django.urls import path
+from django.urls import path, include
+from products import views
 
 
-app_name="products"
+app_name = "products"
+
 urlpatterns = [
-    
+    path("read/<int:pk>/", views.read, name="read"),
+    path("create/", views.create, name="create"),
+    path("update/<int:pk>/", views.update, name="update"),
+    path("delete/<int:pk>/", views.delete, name="delete"),
+
 ]
