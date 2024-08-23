c3413b6385e87fd2fd30019dfa8727307b88c125
SHY-89 <tjduwkrn@gmail.com>
2024-08-23 15:18:43 +0900

    eroor database
    database init error 수정

--- products/forms.py
+++ products/forms.py
@@ -5,7 +5,7 @@ class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = "__all__"
-        exclude = ('uuid',)
+        exclude = ('uuid', 'like_users')
         labels = {
             "title": "제목",
             "content": "설명",
