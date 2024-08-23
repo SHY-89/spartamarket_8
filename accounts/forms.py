ebc92ad4ace70f100e75d775364f9430c9ef0cbf
t2023-m0134 <t2023-m0134@t2023-m0134ui-MacBookAir.local>
2024-08-23 11:00:02 +0900

    feature: 회원정보변경, 비밀번호 변경 기능추가

--- accounts/forms.py
+++ accounts/forms.py
@@ -10,6 +10,28 @@ class ArticleForm(forms.Form):
     content = forms.CharField()
 
 
+class CustomAuthenticationForm(AuthenticationForm):
+    username = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',  
+                'placeholder': '아이디를 입력하세요', 
+            }
+        ),
+        label=''  
+    )
+    password = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                'class': 'form-control',  
+                'placeholder': '비밀번호를 입력하세요',  
+            }
+        ),
+        label=''  
+    )
+
+
+
 class CustomUserCreationForm(UserCreationForm):  
    
    class Meta:
@@ -31,10 +53,21 @@ class CustomUserCreationForm(UserCreationForm):
 
 class CustomAuthenticationForm(AuthenticationForm):
 
+
     username = forms.CharField(
         label=_("아이디"),
         widget=forms.TextInput(attrs={"autofocus": True}),
     )
 
 class CustomUserChangeForm(UserChangeForm):
-    pass
+    password = None
+    
+    class Meta:
+        
+        model = get_user_model()
+        fields = ("first_name", "last_name", "email")
+        labels = {
+            "first_name": "성함",
+            "last_name": "닉네임",
+            "email": "email",
+        }
