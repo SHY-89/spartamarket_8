a4f20f7ceb76d826c846da6139adf4970338ef2a
t2023-m0134 <t2023-m0134@t2023-m0134ui-MacBookAir.local>
2024-08-25 16:09:35 +0900

    댓글달기 기능 구현 완료

--- products/urls.py
+++ products/urls.py
@@ -11,4 +11,6 @@ urlpatterns = [
     path("delete/<int:pk>/", views.delete, name="delete"),
     path("<int:pk>/like/", views.like, name="like"),
     path("update_cnt/", views.update_cnt, name="update_cnt"),
+    path("<int:pk>/comments/", views.comment_create, name="comment_create"),
+    path('<int:pk>/comments/delete/', views.delete_comment, name='delete_comment'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from .forms import ProductForm
-from .models import Product
+from .forms import ProductForm, CommentForm
+from .models import Product, Comment
 from django.views.decorators.http import require_http_methods,require_POST
 from django.db.models import Q
 from django.http import JsonResponse
+
 
 
 def index(request):
@@ -40,9 +41,13 @@ def create(request):
 
 
 def read(request, pk):
-    product = get_object_or_404(Product,pk=pk)
+    product = get_object_or_404(Product, pk=pk)
+    comment_form = CommentForm()
+    comments = product.comments.all().order_by('-created_at')
     context = {
-        'product': product
+        'product': product,
+        'comment_form': comment_form,
+        'comments': comments,
     }
     return render(request, "products/read.html", context)
 
@@ -74,6 +79,7 @@ def delete(request, pk):
         product.delete()
     return redirect("index")
 
+
 @require_POST
 def like(request, pk):
     if request.user.is_authenticated:
@@ -96,3 +102,32 @@ def update_cnt(request):
         "data": '200',
     }
     return JsonResponse(context)
+
+
+def comment_create(request, pk):
+    if request.method == 'POST':
+        product = Product.objects.get(pk=pk)
+        text = request.POST.get('text')
+        author = request.user
+        comment = Comment.objects.create(product=product, text=text, author=author)
+        
+        # 댓글 최신순 정렬
+        comments = Comment.objects.filter(product=product).order_by('-created_at')
+        comments_data = [
+            {
+                'text': comment.text,
+                'author': comment.author.username,
+                'created_at': comment.created_at.isoformat()  # ISO 8601 형식으로
+            }
+            for comment in comments
+        ]
+        
+        return JsonResponse({'comments': comments_data})
+    
+
+@require_POST
+def delete_comment(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    if comment.author == request.user:
+        comment.delete()
+    return JsonResponse({'status': 'success'})
