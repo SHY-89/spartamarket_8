385341755920b86b50f546c442819671be084ee4
SHY-89 <tjduwkrn@gmail.com>
2024-08-26 19:32:33 +0900

    doc readme
    readme file update
    - api 문서 추가
    - 와이어프레임 추가
    - 역활 분담 추가
    
    accounts/ursl.py
    accounts/views.py
    
    안쓰는 index 삭제

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,8 +5,6 @@ from accounts import views
 app_name = "accounts"
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("index/", views.index, name="index"),
     path("login/", views.login, name="login"),
     path("logout/", views.logout, name="logout"),
     path("signup/", views.signup, name="signup"),
